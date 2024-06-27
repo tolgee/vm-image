c13b2337f8716f72ce9ee289faa16fa05340901a
Jan Cizmar <cizmar@chlupac.com>
2024-06-27 14:15:20 +0200

    Add all

---
+++ README.md
@@ -0,0 +1,33 @@
+# Tolgee VM Image tools
+
+This repository contains tools for creating and managing Tolgee VM images.
+Currently, this repository is tailored to Azure Virtual Machines.
+
+## The setup (for Azure)
+When creating the new VM you need to
+1. Create the new Virtual Machine in Azure
+2. Clone contents of this repo to the `/tolgee` directory
+3. Run the `sudo bash ./install.sh`
+
+## The installation script
+
+You can run
+
+```bash
+sudo ./install
+```
+
+to
+
+1. install docker
+2. create configs (`docker-compose.yaml` and `congig.yaml`) with generated postgres password
+3. run the docker containers
+
+## The reset script
+
+The reset script is useful for debugging. It removes the containers and all the data and rerun the configs generating
+script.
+
+```bash
+./reset.sh
+```
---
+++ generateConfig.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Function to generate a random alphanumeric string of a specified length
+generate_random_string() {
+    local length="$1"
+    tr -dc 'A-Za-z0-9' </dev/urandom | head -c "$length" || true
+}
+
+# Length of the random string, default is 12
+length=32
+
+# Files to be processed
+files=("templates/config.template.yaml" "templates/docker-compose.template.yaml")
+
+# Generate a random string
+random_string=$(generate_random_string "$length")
+
+for file in "${files[@]}"; do
+    # Check if the file exists
+    if [ ! -f "$file" ]; then
+        echo "Error: File '$file' not found!"
+        continue
+    fi
+
+    # Output file path (current working directory without '.template' suffix)
+    output_file="${file#templates/}"
+    output_file="${output_file%.template.yaml}.yaml"
+
+    # Replace placeholders and create output file in the current directory
+    sed "s/{postgresPassword}/$random_string/g" "$file" > "$output_file"
+
+    echo "Processed '$file' and saved as '$output_file'."
+done
+
+echo "Replacement completed in all files."
